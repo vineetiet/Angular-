def3d555cee5bea57ece68e3c287452ace8cb25e
vineet gupta <vineet gupta>
2016-12-11 20:24:30 -0600

    chapter 9

---
+++ chapter-9/app.js
@@ -0,0 +1,47 @@
+
+//setup the watcher manually
+(function(){
+	'use strict';
+
+	angular.module('CounterApp', [])
+	.controller('CounterController', CounterController);
+
+	CounterController.$inject = ['$scope'];
+
+	function CounterController($scope){
+
+		$scope.onceCounter = 0;
+		$scope.counter = 0;
+
+		$scope.showNumberOfWatchers = function(){
+
+			console.log("# of watchers: ",$scope.$$watchersCount);
+
+		};
+
+		$scope.countOnce = function(){
+
+			$scope.onceCounter = 1;
+		};
+
+		$scope.upCounter = function(){
+			$scope.counter++;
+
+		};
+
+		$scope.$watch('onceCounter', function(newValue, oldValue){
+
+			console.log("onceCounter old  value: ", oldValue);
+			console.log("onceCounter new value: ", newValue); 	
+
+		});
+
+		$scope.$watch('counter', function(newValue, oldValue){
+
+			console.log("counter old value: ", oldValue);
+			console.log("counter new value: ", newValue); 	
+
+		});
+	}
+
+})();
